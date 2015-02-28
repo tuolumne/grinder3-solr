169c567b52ceb8f9801eb9d57a015f1da4cb6bb0
Sven Maier <sven.maier@library.ucsf.edu>
2015-02-27 17:23:28 -0800

    Update script location

--- setGrinderEnv.sh
+++ setGrinderEnv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 GRINDERPATH=/home/maier/apps/grinder
-GRINDERPROPERTIES=/home/maier/apps/grinder/grinder.properties
+GRINDERPROPERTIES=grinder.properties
 CLASSPATH=$GRINDERPATH/lib/grinder.jar:$CLASSPATH
 JAVA_HOME=/opt/jdk_latest
 PATH=$JAVA_HOME/bin:$PATH
